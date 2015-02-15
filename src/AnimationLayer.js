280c7a6412ba3effcaaa9421ed72b14907e9a56d
MarianYakubets <marian.yakubets@gmail.com>
2015-02-15 21:24:29 +0200

    h3

--- src/AnimationLayer.js
+++ src/AnimationLayer.js
@@ -11,15 +11,25 @@ var AnimationLayer = cc.Layer.extend({
         var winSize = cc.director.getWinSize();
         var centerPos = cc.p(winSize.width / 2, winSize.height / 2);
         draw.setDrawColor(cc.color(255,255,255));
+        draw.setPosition(centerPos)
         var edges = [centerPos];
-        var topR = 60;
+        var topR = 160;
         var startAngle = 0;
-        for (var i = 0; i > 90; i++) {
-            edges.push(cc.p(topR * Math.cos(((i + startAngle) * Math.PI) / 180 + centerPos.x),
-                centerPos.y - topR * Math.sin(((i + startAngle) * Math.PI) / 180)));
+        for (var i = 0; i < 200; i++) {
+            edges.push(cc.p(topR * Math.cos(((i + startAngle) * Math.PI) / 180),
+                - topR * Math.sin(((i + startAngle) * Math.PI) / 180)));
         }
         edges.push(centerPos);
-        draw.drawPoly(edges,cc.color(0,255,255,255),3,cc.color(0,255,255,255));
+        draw.drawPoly(edges,cc.color(0,0,255,255),3,cc.color(0,0,255,255));
+        var edges2 = [centerPos];
+        var topR = 140;
+        var startAngle = 200;
+        for (var i = 0; i < 160; i++) {
+            edges2.push(cc.p(topR * Math.cos(((i + startAngle) * Math.PI) / 180),
+                - topR * Math.sin(((i + startAngle) * Math.PI) / 180)));
+        }
+        edges2.push(centerPos);
+        draw.drawPoly(edges2,cc.color(0,100,255,100),3,cc.color(0,255,0,255));
 
     }
 });
