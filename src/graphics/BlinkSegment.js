fec742a00e99b3b38bb33ebc7da2a2a1ee6b0276
MarianYakubets <marian.yakubets@gmail.com>
2015-03-31 22:18:15 +0300

    home

--- src/graphics/BlinkSegment.js
+++ src/graphics/BlinkSegment.js
@@ -1,16 +1,24 @@
-var BlinkSegment = cc.DrawNode.extend({
-    white: cc.color(255, 255, 255),
-    transparent: cc.color(255, 255, 255, 0),
+var BlinkSegment = cc.Node.extend({
+    white: cc.color(255, 255, 255, 100),
     segment: null,
+    canvas: null,
     ctor: function (segment) {
-        this.segment = segment;
         this._super();
-        this.drawPoly(Utils.getEdges(segment, 1000), this.white, -100, this.white);
+        this.canvas = new cc.DrawNode();
+        this.canvas.drawPoly(Utils.getEdges(segment, 1000), this.white, -100, this.white);
     },
 
     blink: function () {
-        var delay = cc.delayTime(2.0);
-        var setTransparent = this.setPositionZ(-1000);
-        this.runAction(cc.sequence(delay, this.clear(), setTransparent));
+        var add = cc.callFunc(this.callbackAdd, this);
+        var remove = cc.callFunc(this.callbackRemove, this);
+        this.runAction(cc.sequence(add, cc.delayTime(.1), remove));
+    },
+
+    callbackAdd: function () {
+        this.addChild(this.canvas);
+    },
+
+    callbackRemove: function () {
+        this.removeChild(this.canvas);
     }
 });
